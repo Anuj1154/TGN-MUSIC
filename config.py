521332b7b931b33c628f9161934cf468a21fdcae
Anuj1154 <158982341+Anuj1154@users.noreply.github.com>
2024-03-04 17:15:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -83,13 +83,13 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/00012296d728899ad9b56.jpg"
+    "START_IMG_URL", "https://graph.org/file/d731675fd66044d9a0d94.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://telegra.ph/file/00012296d728899ad9b56.jpg"
+    "PING_IMG_URL", "https://graph.org/file/d731675fd66044d9a0d94.jpg"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
-STATS_IMG_URL = "https://telegra.ph/file/00012296d728899ad9b56.jpg"
+STATS_IMG_URL = "https://graph.org/file/d731675fd66044d9a0d94.jpg"
 TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
